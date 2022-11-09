b9391bf39d30735213442ffbbaf681fad7476961
LukeJessop <jessop.lucas@gmail.com>
2022-11-09 10:47:55 -0700

    extra commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Compressor from "compressorjs";
 
 function App() {
+  const [firstImage, setFirstImage] = useState();
+  const [firstImageSize, setFirstImageSize] = useState();
+
+  const [compressedImage, setCompressedImage] = useState();
+  const [compressedImageSize, setCompressedImageSize] = useState();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let imgFile = e.target.files[0];
+    setStateImages(imgFile, "first");
+  };
+
+  const setStateImages = (imgFile, state) => {
+    let reader = new FileReader();
+    reader.readAsDataURL(imgFile);
+
+    reader.onload = (event) => {
+      let img_url = event.target.result;
+      if (state === "first") {
+        console.log(imgFile.size);
+        setFirstImage(img_url);
+        setFirstImageSize(imgFile.size);
+        compressFile(imgFile, img_url);
+      }
+      if (state === "compressed") {
+        console.log(imgFile.size);
+        setCompressedImage(img_url);
+        setCompressedImageSize(imgFile.size);
+      }
+    };
+  };
+
+  const compressFile = (file, img_url) => {
+    new Compressor(file, {
+      quality: 0.1,
+      success(result) {
+        setStateImages(result, "compressed");
+      }
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="upload-container">
+        <div>
+          <h3>Upload file!</h3>
+          <input
+            type="file"
+            accept=".jpg, .jpeg, .png, .gif, .mp4"
+            onChange={(e) => {
+              submitHandler(e);
+            }}
+          />
+        </div>
+      </div>
+
+      <br />
+      <div className="image-container">
+        <div>
+          <h2>Before Compression</h2>
+          <img src={firstImage} />
+          <h3>Size: {Math.floor(firstImageSize / 1000)}kb</h3>
+        </div>
+        <div>
+          <p>quality</p>
+          <input type="range" min="0" max="10" />
+          <h2>After Compression</h2>
+          <img src={compressedImage} />
+          <h3>Size: {Math.floor(compressedImageSize / 1000)}kb</h3>
+        </div>
+      </div>
+      <br />
+      <div>
+        <h1>Visual Example uses for compressed image</h1>
+      </div>
     </div>
   );
 }
